2d47f298e637a276fc9199dda17d8b118622ca53
Micah Levine <micah.levine@outlook.com>
2019-08-11 13:35:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,10 +3,21 @@
 def countdown(number)
   while number > 0
     sleep 1
-    puts "#{number} SECONDS!"
+    puts "#{number} SECOND(S)!"
     number -= 1
   end
     return "HAPPY NEW YEAR!"
 end
+
+countdown(10)
+
+def countdown_with_sleep(number)
+  while number > 0
+    sleep 1
+    puts "#{number} SECOND(S)"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end 
 
 countdown(10)
