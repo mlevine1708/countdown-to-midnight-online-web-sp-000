7db5422358924e74601a36a4c9ff4d31e80cee01
Micah Levine <micah.levine@outlook.com>
2019-08-11 13:32:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,9 @@
 #write your code here
 
 def countdown(number)
-  number = 10
-  until number == 0
+  while number > 0
     puts "#{number} SECONDS!"
     number -= 1
-    number == 0
-    puts "HAPPY NEW YEAR!"
   end
+    return "HAPPY NEW YEAR!"
 end
